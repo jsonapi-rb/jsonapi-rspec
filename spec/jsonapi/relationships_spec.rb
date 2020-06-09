b7151af99d7a792e9b607b62a743dbc0288a0c5a
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-09 10:27:56 +1000

    Create relationships spec as non-exists.

---
+++ spec/jsonapi/relationships_spec.rb
@@ -0,0 +1,40 @@
+RSpec.describe JSONAPI::RSpec do
+  let(:doc) do
+    {
+      'relationships' => {
+        'posts' => {
+          'data' => {
+            'id' => '1',
+            'type' => 'posts'
+          }
+        },
+        'comments' => {
+          'data' => [{
+            'id' => '1',
+            'type' => 'posts'
+          }, {
+            'id' => '2',
+            'type' => 'hides'
+          }]
+        }
+      }
+    }
+  end
+
+  describe '#have_jsonapi_attributes' do
+    it { expect(doc).to have_relationship(:posts) }
+    it { expect(doc).not_to have_relationship(:mails) }
+    it { expect(doc).to have_relationship(:posts).with_data({ 'id' => '1', 'type' => 'posts' }) }
+    it do
+      expect(doc).to have_relationship(:comments).with_data(
+        [{ 'id' => '1', 'type' => 'posts' }, { 'id' => '2', 'type' => 'hides' }]
+      )
+    end
+  end
+
+  describe '#have_relationships' do
+    it { expect(doc).to have_relationships(:posts, :comments) }
+    it { expect(doc).to have_relationships('posts', 'comments') }
+    it { expect(doc).not_to have_relationships(:posts, :comments, :mails) }
+  end
+end
