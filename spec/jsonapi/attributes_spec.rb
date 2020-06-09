265d2eda7d773adf0ba3c535568808c653c0a6e5
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-09 11:38:59 +1000

    Add string key tests to attributes and links matchers
    For consistency, as all other specs now contain tests for symbols and strings, added string tests into the attributes and links specs.

--- spec/jsonapi/attributes_spec.rb
+++ spec/jsonapi/attributes_spec.rb
@@ -11,13 +11,19 @@ RSpec.describe JSONAPI::RSpec do
 
   describe '#have_attribute' do
     it { expect(doc).to have_attribute(:one) }
+    it { expect(doc).to have_attribute('one') }
     it { expect(doc).not_to have_attribute(:five) }
+    it { expect(doc).not_to have_attribute('five') }
   end
 
   describe '#have_jsonapi_attributes' do
     it { expect(doc).to have_jsonapi_attributes(:one, :two) }
+    it { expect(doc).to have_jsonapi_attributes('one', 'two') }
     it { expect(doc).not_to have_jsonapi_attributes(:two, :five) }
+    it { expect(doc).not_to have_jsonapi_attributes('two', 'five') }
     it { expect(doc).to have_jsonapi_attributes(:one, :two, :four).exactly }
-    it { expect(doc).not_to have_jsonapi_attributes(:one).exactly }
+    it { expect(doc).to have_jsonapi_attributes('one', 'two', 'four').exactly }
+    it { expect(doc).not_to have_jsonapi_attributes('one').exactly }
+    it { expect(doc).not_to have_jsonapi_attributes('one').exactly }
   end
 end
--- spec/jsonapi/links_spec.rb
+++ spec/jsonapi/links_spec.rb
@@ -11,12 +11,18 @@ RSpec.describe JSONAPI::RSpec do
   context '#have_link' do
     it { expect(doc).to have_link(:self) }
     it { expect(doc).to have_link(:self).with_value('self_link') }
+    it { expect(doc).to have_link('self') }
+    it { expect(doc).to have_link('self').with_value('self_link') }
     it { expect(doc).not_to have_link(:self).with_value('any_link') }
     it { expect(doc).not_to have_link(:any) }
+    it { expect(doc).not_to have_link('self').with_value('any_link') }
+    it { expect(doc).not_to have_link('any') }
   end
 
   context '#have_links' do
     it { expect(doc).to have_links(:self, :related) }
     it { expect(doc).not_to have_links(:self, :other) }
+    it { expect(doc).to have_links('self', 'related') }
+    it { expect(doc).not_to have_links('self', 'other') }
   end
 end
