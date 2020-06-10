569cd6a1ab60450b3b2abc7b516f7988f691b415
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-10 17:28:32 +1000

    Enable Symbol parameter for type matcher
    Add support for using a symbol as the type and create related spec tests.

--- lib/jsonapi/rspec/type.rb
+++ lib/jsonapi/rspec/type.rb
@@ -2,7 +2,7 @@ module JSONAPI
   module RSpec
     module Type
       ::RSpec::Matchers.define :have_type do |expected|
-        match { |actual| actual['type'] == expected }
+        match { |actual| actual['type'] == expected.to_s }
       end
     end
   end
--- spec/jsonapi/type_spec.rb
+++ spec/jsonapi/type_spec.rb
@@ -2,9 +2,16 @@ RSpec.describe JSONAPI::RSpec, '#have_type' do
   it 'succeeds when type matches' do
     expect('type' => 'foo').to have_type('foo')
   end
+  it 'succeeds when type (as symbol) matches' do
+    expect('type' => 'foo').to have_type(:foo)
+  end
 
   it 'fails when type mismatches' do
     expect('type' => 'foo').not_to have_type('bar')
+  end
+
+  it 'fails when type (as symbol) mismatches' do
+    expect('type' => 'foo').not_to have_type(:bar)
   end
 
   it 'fails when type is absent' do
