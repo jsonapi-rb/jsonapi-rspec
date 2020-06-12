016868295713dab5211a70083c7487d60510c629
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-12 11:09:42 +1000

    Create allow_symbolized_jsonapi config and  as_indifferent_hash method
    Create configuration setting to allow the use of HashWithIndifferentAccess and symbolized hashes as source documents to match again.
    
    Incorporate as_different_hash method into JSONAPI::RSpec to provide a common utility to convert source documents into a known format (if allow_symbolized_jsonapi is enabled)

--- lib/jsonapi/rspec.rb
+++ lib/jsonapi/rspec.rb
@@ -7,6 +7,10 @@ require 'jsonapi/rspec/links'
 require 'jsonapi/rspec/meta'
 require 'jsonapi/rspec/jsonapi_object'
 
+RSpec.configure do |c|
+  c.add_setting :allow_symbolized_jsonapi, default: false
+end
+
 module JSONAPI
   module RSpec
     include Id
@@ -16,5 +20,12 @@ module JSONAPI
     include Links
     include Meta
     include JsonapiObject
+
+    def self.as_indifferent_hash(doc)
+      return doc unless ::RSpec.configuration.allow_symbolized_jsonapi
+      return doc.with_indifferent_access if doc.respond_to?(:with_indifferent_access)
+
+      JSON.parse(JSON.generate(doc))
+    end
   end
 end
