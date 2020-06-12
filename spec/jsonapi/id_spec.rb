ab380704e72d4babdacc2c7e40ef902fd5f068ff
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-12 11:35:52 +1000

    Standardise id spec and add symbolized source doc
    Refactored the id spec to align it with the pattern provided in the links and attributes specs.  Incorporate symbolized source document tests inline with the attributes and relationships specs

--- spec/jsonapi/id_spec.rb
+++ spec/jsonapi/id_spec.rb
@@ -1,13 +1,17 @@
 RSpec.describe JSONAPI::RSpec, '#have_id' do
-  it 'succeeds when id matches' do
-    expect('id' => 'foo').to have_id('foo')
-  end
+  json_doc = { 'id' => 'foo' }
+  symbol_doc = { id: 'foo' }
 
-  it 'fails when id mismatches' do
-    expect('id' => 'foo').not_to have_id('bar')
-  end
+  describe '#have_id' do
+    [json_doc, symbol_doc].each do |doc|
+      subject { doc }
+      before(:each) { RSpec.configuration.allow_symbolized_jsonapi = (doc == symbol_doc) }
 
-  it 'fails when id is absent' do
-    expect({}).not_to have_id('foo')
+      it { is_expected.to have_id('foo') }
+      it { is_expected.not_to have_id('bar') }
+    end
+    it 'is expected not to have id "foo" when id is not present' do
+      expect({}).not_to have_id('foo')
+    end
   end
 end
