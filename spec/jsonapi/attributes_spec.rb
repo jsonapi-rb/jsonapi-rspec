894e393bd0e5e2c9894c58df7be02e43aba61b36
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-12 11:22:50 +1000

    Refactor attributes spec
    Refactor attributes spec to remove the it clauses so the standard test output is visible in documentation mode.  Added present and not present tests to align with other existing tests.

--- spec/jsonapi/attributes_spec.rb
+++ spec/jsonapi/attributes_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe JSONAPI::RSpec do
-  let(:doc) do
+  json_doc =
     {
       'attributes' => {
         'one' => 1,
@@ -7,17 +7,28 @@ RSpec.describe JSONAPI::RSpec do
         'four' => 3
       }
     }
-  end
 
   describe '#have_attribute' do
-    it { expect(doc).to have_attribute(:one) }
-    it { expect(doc).not_to have_attribute(:five) }
+    context 'when attributes is present' do
+      it { expect(json_doc).to have_attribute(:one) }
+      it { expect(json_doc).not_to have_attribute(:five) }
+    end
+
+    context 'when attributes is not present' do
+      it { expect({}).not_to have_attribute(:one) }
+    end
   end
 
   describe '#have_jsonapi_attributes' do
-    it { expect(doc).to have_jsonapi_attributes(:one, :two) }
-    it { expect(doc).not_to have_jsonapi_attributes(:two, :five) }
-    it { expect(doc).to have_jsonapi_attributes(:one, :two, :four).exactly }
-    it { expect(doc).not_to have_jsonapi_attributes(:one).exactly }
+    context 'when attributes is present' do
+      it { expect(json_doc).to have_jsonapi_attributes(:one, 'two') }
+      it { expect(json_doc).not_to have_jsonapi_attributes(:two, 'five') }
+      it { expect(json_doc).to have_jsonapi_attributes(:one, :two, 'four').exactly }
+      it { expect(json_doc).not_to have_jsonapi_attributes(:one).exactly }
+    end
+
+    context 'when attributes is not present' do
+      it { expect({}).not_to have_jsonapi_attributes(:one, 'two') }
+    end
   end
 end
