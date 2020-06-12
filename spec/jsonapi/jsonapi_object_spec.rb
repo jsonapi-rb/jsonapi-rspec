988bc9ef4a218c6e2116c2761edda7f5a1249ead
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-12 11:44:20 +1000

    Refactor jsonapi spec
    Refactor jsonapi spec to remove the it clauses so the standard test output is visible in documentation mode.  Added present and not present tests to align with other existing tests.

--- spec/jsonapi/jsonapi_object_spec.rb
+++ spec/jsonapi/jsonapi_object_spec.rb
@@ -1,27 +1,21 @@
-RSpec.describe JSONAPI::RSpec, '#have_jsonapi_object' do
-  context 'when providing no value' do
-    it 'succeeds when jsonapi object is present' do
-      expect('jsonapi' => { 'version' => '1.0' }).to have_jsonapi_object
+RSpec.describe JSONAPI::RSpec do
+  json_doc =
+    {
+      'jsonapi' => {
+        'version' => '1.0'
+      }
+    }
+
+  describe '#have_jsonapi_object' do
+    context 'when jsonapi is present' do
+      it { expect(json_doc).to have_jsonapi_object }
+      it { expect(json_doc).to have_jsonapi_object('version' => '1.0') }
+      it { expect(json_doc).not_to have_jsonapi_object('version' => '2.0') }
     end
 
-    it 'fails when jsonapi object is absent' do
-      expect({}).not_to have_jsonapi_object
-    end
-  end
-
-  context 'when providing a value' do
-    it 'succeeds when jsonapi object matches' do
-      expect('jsonapi' => { 'version' => '1.0' })
-        .to have_jsonapi_object('version' => '1.0')
-    end
-
-    it 'fails when jsonapi object mismatches' do
-      expect('jsonapi' => { 'version' => '2.0' })
-        .not_to have_jsonapi_object('version' => '1.0')
-    end
-
-    it 'fails when jsonapi object is absent' do
-      expect({}).not_to have_jsonapi_object('version' => '1.0')
+    context 'when jsonapi is not present' do
+      it { expect({}).not_to have_jsonapi_object }
+      it { expect({}).not_to have_jsonapi_object('version' => '1.0') }
     end
   end
 end
