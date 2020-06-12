865ff4bda5ad82f6aafedca64b14330211b345cd
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-12 12:37:14 +1000

    Refactor meta spec
    Refactor meta spec to remove the it clauses so the standard test output is visible in documentation mode.  Added present and not present tests to align with other existing tests.

--- spec/jsonapi/meta_spec.rb
+++ spec/jsonapi/meta_spec.rb
@@ -1,25 +1,21 @@
-RSpec.describe JSONAPI::RSpec, '#have_meta' do
-  context 'when providing no value' do
-    it 'succeeds when meta is present' do
-      expect('meta' => {}).to have_meta
+RSpec.describe JSONAPI::RSpec do
+  json_doc =
+    {
+      'meta' => {
+        'foo' => 'bar'
+      }
+    }
+
+  describe '#have_meta' do
+    context 'when meta is present' do
+      it { expect(json_doc).to have_meta }
+      it { expect(json_doc).to have_meta('foo' => 'bar') }
+      it { expect(json_doc).not_to have_meta('foo' => 'baz') }
     end
 
-    it 'fails when meta is absent' do
-      expect({}).not_to have_meta
-    end
-  end
-
-  context 'when providing a value' do
-    it 'succeeds when meta matches' do
-      expect('meta' => { foo: 'bar' }).to have_meta(foo: 'bar')
-    end
-
-    it 'fails when meta mismatches' do
-      expect('meta' => { foo: 'bar' }).not_to have_meta(bar: 'baz')
-    end
-
-    it 'fails when meta is absent' do
-      expect({}).not_to have_meta(foo: 'bar')
+    context 'when meta is not present' do
+      it { expect({}).not_to have_meta }
+      it { expect({}).not_to have_meta('foo' => 'bar') }
     end
   end
 end
