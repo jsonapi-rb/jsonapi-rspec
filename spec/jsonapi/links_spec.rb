1cc464cafd40d2d324a5075b728722128e83a4b7
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-26 11:10:04 +1000

    Refactor links spec
    Refactor links spec to remove the it clauses so the standard test output is visible in documentation mode.  Added present and not present tests to align with other existing tests.

--- spec/jsonapi/links_spec.rb
+++ spec/jsonapi/links_spec.rb
@@ -1,22 +1,33 @@
 RSpec.describe JSONAPI::RSpec do
-  let(:doc) do
+  json_doc =
     {
       'links' => {
         'self' => 'self_link',
         'related' => 'related_link'
       }
     }
+
+  describe '#have_link' do
+    context 'when links is present' do
+      it { expect(json_doc).to have_link(:self) }
+      it { expect(json_doc).to have_link(:self).with_value('self_link') }
+      it { expect(json_doc).not_to have_link(:self).with_value('any_link') }
+      it { expect(json_doc).not_to have_link(:any) }
+    end
+
+    context 'when links is not present' do
+      it { expect({}).not_to have_link(:self) }
+    end
   end
 
-  context '#have_link' do
-    it { expect(doc).to have_link(:self) }
-    it { expect(doc).to have_link(:self).with_value('self_link') }
-    it { expect(doc).not_to have_link(:self).with_value('any_link') }
-    it { expect(doc).not_to have_link(:any) }
-  end
+  describe '#have_links' do
+    context 'when links is present' do
+      it { expect(json_doc).to have_links(:self, 'related') }
+      it { expect(json_doc).not_to have_links(:self, 'other') }
+    end
 
-  context '#have_links' do
-    it { expect(doc).to have_links(:self, :related) }
-    it { expect(doc).not_to have_links(:self, :other) }
+    context 'when links is not present' do
+      it { expect({}).not_to have_links(:self, 'related') }
+    end
   end
 end
