5116a07bf88190b5f86848e3effdf3ef14723fa7
Stephen Anderson <stephen.anderson@appstech.com.au>
2020-06-26 11:12:39 +1000

    Refactor id spec
    Refactor id spec to remove the it clauses so the standard test output is visible in documentation mode.  Added present and not present tests to align with other existing tests.

--- spec/jsonapi/id_spec.rb
+++ spec/jsonapi/id_spec.rb
@@ -1,17 +1,16 @@
 RSpec.describe JSONAPI::RSpec, '#have_id' do
-  json_doc = { 'id' => 'foo' }
-  symbol_doc = { id: 'foo' }
+  json_doc =
+    {
+      'id' => 'foo'
+    }
 
   describe '#have_id' do
-    [json_doc, symbol_doc].each do |doc|
-      subject { doc }
-      before(:each) { RSpec.configuration.allow_symbolized_jsonapi = (doc == symbol_doc) }
-
-      it { is_expected.to have_id('foo') }
-      it { is_expected.not_to have_id('bar') }
+    context 'when id is present' do
+      it { expect(json_doc).to have_id('foo') }
+      it { expect(json_doc).not_to have_id('bar') }
     end
-    it 'is expected not to have id "foo" when id is not present' do
-      expect({}).not_to have_id('foo')
+    context 'when id is not present' do
+      it { expect({}).not_to have_id('foo') }
     end
   end
 end
