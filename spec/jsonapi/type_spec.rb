fec5cd8b2ccf3bcd72127fc15094fdef2d6b53f2
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2020-08-28 11:48:55 +0200

    Add context for mismatching type classes to have_type spec

--- spec/jsonapi/type_spec.rb
+++ spec/jsonapi/type_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe JSONAPI::RSpec, '#have_type' do
     expect('type' => 'foo').to have_type('foo')
   end
 
+  context 'with mismatching classes' do
+    it 'succeeds when type is a symbol' do
+      expect('type' => :foo).to have_type('foo')
+    end
+
+    it 'succeeds when argument is a symbol' do
+      expect('type' => 'foo').to have_type(:foo)
+    end
+  end
+
   it 'fails when type mismatches' do
     expect('type' => 'foo').not_to have_type('bar')
   end
